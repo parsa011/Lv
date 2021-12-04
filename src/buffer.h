90411e64555794d7d3731d8e298542a5d5a361c5
parsa011 <parsa32569@gmail.com>
2021-12-04 17:06:02 +0330

    add some mode for window and buffer structrus

--- src/buffer.h
+++ src/buffer.h
@@ -9,8 +9,28 @@
  *	This file is part of Lv
  */
 
+#define NFILEN  256		/* n of bytes, file name 	*/
+#define NBUFN   16		/* n of bytes, buffer name 	*/
+
 typedef struct buffer_t {
-	line *first_line;		/* first line of buffer , to get doubly-linked list of lines */
-} buffer;
+	line *fline;			/* first line of buffer , to get doubly-linked list of lines */
+	line *hline;			/* header line in this view (or page) 						 */
+	line *cline;			/* current line in buffer (where the cursor is) 			 */
+	char fname[NFILEN];		/* file name 												 */
+	char bname[NBUFN];		/* buffer name 												 */
+	char flag;				/* flags 													 */
+	char mode;				/* modes of this buffer 									 */
+	int loffset;			/* paased lines 											 */
+};
+
+#define	MDLOCK	0x0001		/* lock mode                     */
+#define	MDCMOD	0x0002		/* c indentation and fence match */
+#define	MDSPELL	0x0004		/* spell error parcing           */
+#define	MDEXACT	0x0008		/* exact matching for searches   */
+#define	MDVIEW	0x0010		/* read-only buffer              */
+#define MDOVER	0x0020		/* overwrite mode                */
+#define MDMAGIC	0x0040		/* regular expresions in search  */
+#define	MDCRYPT	0x0080		/* encrytion mode active         */
+#define	MDASAVE	0x0100		/* auto-save mode                */
 
 #endif
--- src/window.h
+++ src/window.h
@@ -15,16 +15,24 @@
  * 	redisplay
  */
 struct window_t {
-	LINK(window) link;		/* Doubly-linked list of windows */
-	buffer *buffers;		/* buffers in this window */
-	int buffers_count;		/* total buffers count in this window */
-	buffer *cb;				/* current buffer (active) */
-	int cb_index;			/* index of cb */	
-	char flags;				/* flags that holds state of window */
-	char *wname;			/* window name for show in tabs section */
+	LINK(window) link;		/* Doubly-linked list of windows 			*/
+	buffer *buffers;		/* buffers in this window 					*/
+	int bcount;				/* total buffers count in this window 		*/
+	//buffer *cb;			/* current buffer (active) 					*/
+	int cbindex;			/* index of current buffer (active buffer)  */	
+	char flags;				/* flags that holds state of window 		*/
 };
 
-#define wnext(w) (w.link->next)
-#define wprev(w) (w.link->prev)
+#define wnext(w) (w.link->next)				/* next window of given window 		*/
+#define wprev(w) (w.link->prev)				/* prev window of given window 		*/
+#define wcbuffer(w) (w.buffers[w.cb_index]) /* return current buffer in window 	*/
+
+/* window modes */
+#define WFFORCE 0x01		/* window needs forced reframe  */
+#define WFMOVE  0x02		/* movement from line to line   */
+#define WFEDIT  0x04		/* editing within a line        */
+#define WFHARD  0x08		/* better to a full display     */
+#define WFMODE  0x10		/* update mode line.            */
+#define	WFCOLR	0x20		/* needs a color change         */
 
 #endif
