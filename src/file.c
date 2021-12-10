69e4438b5b2ed30a9a0d0472d3c846729642e29a
parsa011 <parsa32569@gmail.com>
2021-12-10 21:43:48 +0330

    eliminate nominal similarity in load file functio change line variable name to line_chars , so now we can use line struct without struct prefix :)

--- src/file.c
+++ src/file.c
@@ -19,21 +19,21 @@ int load_file_into_buffer(buffer *buf,char *filepath)
 		return FNOTF;	
 	/* if given buffer is NULL using curbp instead */
 	buf = buf != NULL ? buf : curbp;
-	char *line = NULL;
+	char *line_chars = NULL;
 	size_t linecap = 0;
 	ssize_t linelen;
-	struct line_t *last_line = NULL;
-	struct line *ln = NULL;
-	while ((linelen = getline(&line, &linecap, fp)) != EOF) { 
+	line *last_line = NULL;
+	line *ln = NULL;
+	while ((linelen = getline(&line_chars, &linecap, fp)) != EOF) { 
 		/* remove extra 'new line' and 'line feed' characters of end of line */
-		while (linelen > 0 && (line[linelen - 1] == '\n' ||	line[linelen - 1] == '\r'))
+		while (linelen > 0 && (line_chars[linelen - 1] == '\n' || line_chars[linelen - 1] == '\r'))
 			linelen--;
 		/* 
 		 * if buffer dont have any line , so this is first line 
 		 * in other hand , we have to append this line to next of last line
 		 * and then set last line to this new line
 		 */
-		ln = line_alloc(line,linelen);
+		ln = line_alloc(line_chars,linelen);
 		if (buf->fline == NULL) {
 			buf->fline = ln;
 			buf->cline = ln;
@@ -42,10 +42,10 @@ int load_file_into_buffer(buffer *buf,char *filepath)
 		}
 		/* i think this line dont need any comment , but to be sure : increase total count of buffer lines */	
 		buf->lline = ln;
-			slprev(buf->lline,last_line);
+		slprev(buf->lline,last_line);
 		buf->lcount++;
 		last_line = ln;
 	}
-	free(line);
+	free(line_chars);
 	fclose(fp);
 }
