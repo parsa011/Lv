ec3c3ccb6fbf84d1c356cdd5b1ce82f315f7ce9f
parsa011 <parsa32569@gmail.com>
2022-01-11 23:29:15 +0330

    some new colors and props

--- src/color.h
+++ src/color.h
@@ -3,6 +3,10 @@
 
 #define DEFAULT "\x1b[0m"
 #define BOLD 	"\x1b[1m"
+#define DIM		"\x1b[2m"
+#define ITALIC	"\x1b[3m"
+#define UNDLINE "\x1b[4m"
+#define BLINK	"\x1b[5m"
 #define INVERT 	"\x1b[7m"
 #define RED 	"\x1b[31m"
 #define GREEN   "\x1b[32m"
@@ -11,6 +15,7 @@
 #define MAGENTA "\x1b[35m"
 #define CYAN    "\x1b[36m"
 #define NORMAL  "\x1b[39m"
+#define OVLINE	"\x1b[53m"
 #define GRAY	"\x1b[90m"
 
 #endif
